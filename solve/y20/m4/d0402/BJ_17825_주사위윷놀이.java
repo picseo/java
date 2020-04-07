6a43d46bb0e2aa93494181addb3f06ebb4aa2e58
Seunghee Suh <pucca94@naver.com>
2020-04-07 22:42:57 +0900

    윷놀이

--- solve/y20/m4/d0402/BJ_17825_주사위윷놀이.java
+++ solve/y20/m4/d0402/BJ_17825_주사위윷놀이.java
@@ -22,7 +22,17 @@ import java.util.Scanner;
  * 
  * 
  * 이동을 마칠때마다 점수가 추가된다.
+ * 
+ * 말판을 자세히 보면 30이 두개 있다
+ * 그런데 이것을 생각하지 못하고 말판에 써있는 값이 30일때 자꾸  t== 3으로 바꿔버리니까 이상한 값이 나왔다.
+ * 이문제에서는 t==0일때만 새로운 루트로 갈 수 있으므로
+ * t==0일때만 if문에서 t의 값을 바꿔주게 했더니 답을 얻을 수 있었다.
+ * 
+ * 또한 말의 중복순열을 dfs를 이용해서 구했는데 이것을 
+ * bitmasking을 이용해서 할 수 있다고 한다.
  */
+
+
 public class BJ_17825_주사위윷놀이 {
 	static int[] num = new int[10];
 	static int result = 0;
@@ -40,10 +50,6 @@ public class BJ_17825_주사위윷놀이 {
 			num[i] = sc.nextInt();
 		}
 		
-		System.out.println(st[0].length);
-		System.out.println(st[1].length);
-		System.out.println(st[2].length);
-		System.out.println(st[3].length);
 		dfs(10, 0, new int[10]);
 		
 		System.out.println(result);
@@ -64,59 +70,59 @@ public class BJ_17825_주사위윷놀이 {
 	}
 
 	private static int count(int[] tmp) {
-		Node[] horse = new Node[4];
-		for(int i = 0; i <4; i++) {
-			horse[i] = new Node();
-		}
+		int[][] horse = new int[4][2]; // 0 : t, 1 : where
 		
-		int res = 0;
-		
+		int res = 0;		
 		for(int i = 0; i < 10; i++) {
-			int t = horse[tmp[i]].t_num;
-			int w = horse[tmp[i]].where;
+			int now = tmp[i]; // 현재 말의 번호
+			int t = horse[now][0];
+			int w = horse[now][1];
 			
 			if(t == -1) {
-				return -1;//도착칸에 도착한 말은 선택할 수 없다.
+				return -1;//도착칸에 도착한 말은 선택할 수 없다. -> 더이상 볼 필요가 없다.
 			}
 			
-			if(w + num[i] < st[t].length) {//범위 내에 존재하는가
+			int wn = w + num[i];
+			if(wn < st[t].length) {//범위 내에 존재하는가
 				for(int j = 0; j < 4; j++) {
-					if(j == tmp[i] || horse[j].t_num == -1)
+					if(j == now || horse[j][0] == -1)
 						continue;
-					if(st[horse[j].t_num][horse[j].where] == st[t][w+num[i]]) {
-						return -1;
+					if(st[horse[j][0]][horse[j][1]] == st[t][wn]) {
+						//16, 22, 24, 26, 28 중복되는 값이므로 다르게 처리해야 한다.
+						if(st[t][wn] == 16||st[t][wn] == 22||st[t][wn] == 24||st[t][wn] == 26||st[t][wn] == 28||st[t][wn] == 30) {
+							if(horse[j][0] == t && horse[j][1] == wn) {
+								return -1;
+							}
+						}else {
+							return -1;//도달하게 되는 칸에 이미 다른 말이 있을 수 없으므로 이 루트는 적합하지 않다.
+						}
 					}
 				}
 				
 				//도착하는 칸에 다른 말이 없다면
-				res += st[t][w+num[i]];
-				if(st[t][w+num[i]] == 10) {
-					horse[tmp[i]].t_num = 1;
-					horse[tmp[i]].where = 0;
-				}else if(st[t][w+num[i]] == 20) {
-					horse[tmp[i]].t_num = 2;
-					horse[tmp[i]].where = 0;
-				}else if(st[t][w+num[i]] == 30) {
-					horse[tmp[i]].t_num = 3;
-					horse[tmp[i]].where = 0;
+				int next = st[t][wn];
+				res += next;
+				if(t == 0 && st[t][wn] == 10) {
+					horse[now][0] = 1;
+					horse[now][1] = 0;
+				}else if(t == 0 &&st[t][wn] == 20) {
+					horse[now][0] = 2;
+					horse[now][1] = 0;
+				}else if(t == 0 &&st[t][wn] == 30) {
+					horse[now][0] = 3;
+					horse[now][1] = 0;
 				}else {
-					horse[tmp[i]].where = w+num[i];
+					horse[now][1] = wn;
 				}				
 			}else {
 			 //범위를 벗어나면 도착칸에 들어간것을 의미한다.
-				horse[tmp[i]].t_num = -1;
+				horse[now][0] = -1;
+				horse[now][1] = -1;
 			}
 			
 		}		
 		
 		return res;
-	}
-
-	static class Node{
-		int t_num = 0;//시작위치
-		int where = 0;//몇번째인지
-		public Node() {
-		}		
 	}
 	
 }
